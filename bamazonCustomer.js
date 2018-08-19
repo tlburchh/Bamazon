8a57fe3b4e7a66b46d96b6daf90b61acb41103fb
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-08-19 16:39:39 -0400

    Initial js connecting to mySQL

---
+++ bamazonCustomer.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log('connected as id ' + connect.threadID + "\n");
+
+
+
+})
